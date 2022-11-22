4df97050abfa0e414d254c6b08966aff1e71ac70
vytautas-dotcom <tumas.vytautas@gmail.com>
2022-11-22 21:45:37 +0200

    Add method to create labyrinth and estimate initial coordinates

---
+++ GameRunner/Labyrinth.cs
@@ -0,0 +1,38 @@
+﻿namespace GameRunner
+{
+    public class Labyrinth
+    {
+        private readonly IDataReader _reader;
+        public List<char[]> map;
+        public int playerPositionX;
+        public int playerPositionY;
+        public Labyrinth(IDataReader reader)
+        {
+            _reader = reader;
+            map = new List<char[]>();
+        }
+
+        public void CreateMap(string path)
+        {
+            var data = _reader.Read(path);
+            int lineCount = 0;
+
+            foreach (string line in data)
+            {
+                var chars = line.ToCharArray();
+                map.Add(chars);
+                int playersXCoordinate = FindPlayersXCoordinate(line);
+
+                if (playersXCoordinate != -1)
+                {
+                    playerPositionX = playersXCoordinate;
+                    playerPositionY = lineCount;
+                }
+                lineCount++;
+            }
+        }
+
+        private static int FindPlayersXCoordinate(string line)
+            => line.IndexOf(LabyrinthConstants.PlayersPosition);
+    }
+}
