92a5b5e4f70462dae773edf3aea16cc2629f662c
vytautas-dotcom <tumas.vytautas@gmail.com>
2022-11-22 21:33:44 +0200

    Initial

---
+++ GameRunner/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace GameRunner;
+
+public class Game : IGame
+{
+    public int Run(string filePath)
+    {
+        // TODO: start your journey here
+
+        return -1;
+    }
+}
---
+++ GameRunner/Program.cs
@@ -0,0 +1,5 @@
+﻿using GameRunner;
+
+IGame game = new Game();
+
+var result = game.Run(@"TestData\map1.txt");
